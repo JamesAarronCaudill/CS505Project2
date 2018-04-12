d44f93524508124dc313613f13fed8f12c8f7cd7
Aarron Caudill <aarron@aarrons-mbp.lan>
2018-04-12 18:36:02 -0400

    Pushing for partner

--- Project2.py
+++ Project2.py
@@ -20,15 +20,43 @@ DirectedEmailGraph = nx.DiGraph()
 def GraphBuilderFunction():
     i = 0
     for x in range(len(myfile) - 1):
-        myTempFile = myfile[i].split(' ')
+        #This is the iterator responsible for placing the list of items in the graph
         j = 1
+
+        #This makes sure that we dont go outside of the range of the list
+        if i <= (len(myfile) - 1):
+
+            #Splits the string based on spaces, this allows us to have individual names
+            myTempFile = myfile[i].split(' ')
+
+            #This removes the last item in the list which happens to be an empty string caused by a space after the last name.
+            myTempFile = myTempFile[:-1]
+
+        #This runs through the size of the a list that is temp. which contains the split file.
         for z in range(len(myTempFile) - 1):
+
+            #This uses iterator j to place everything in the graph as an edge from the first item.
             DirectedEmailGraph.add_edge(myTempFile[0], myTempFile[j])
+
+            #j is increased by one to gain access to the next item
             j = j + 1
+
+        #These are here for the purpose of debugging. Graph is to large to build on its own. Un comment these to see it run through
+        #each like of the dataset.
+        #nx.draw(DirectedEmailGraph, with_labels=True)
+        #plt.show()
+
+        #This increments i plus 2 because every other i is a new line character due to splitting on the new line characters.
         i = i + 2
-        print DirectedEmailGraph.edges()
-        nx.draw(DirectedEmailGraph,with_labels=True)
-        plt.show()
+
+
     return
 
+
+def FindCliques():
+    print find_cliques(DirectedEmailGraph.to_undirected(reciprocal=False))
+    return
+
+#Calls the GraphBuilderFunction
 GraphBuilderFunction()
+FindCliques()
